d925890332f1dbd97a8b4dd83dc0b8721405db92
ian038 <ianphua3@gmail.com>
2022-06-14 13:35:18 -0400

    Added product component

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 
-export default function HeadComponent(): JSX.Element {
+const HeadComponent: React.FC<{}> = () => {
     return (
         <Head>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -27,3 +27,5 @@ export default function HeadComponent(): JSX.Element {
         </Head>
     );
 }
+
+export default HeadComponent
